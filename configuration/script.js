9b4fac9d3bebab4a5b8dcab5560330bd4f3d3cde
marvinheinzelmann <marvin.heinzelmann@t-online.de>
2014-10-21 18:21:21 +0200

    Basic Concept part 2
    The matching script

--- configuration/script.js
+++ configuration/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-
+	$('#flavors-point').click(function(){
+		changeToStep('#flavors');
+	});
+	$('#colors-point').click(function(){
+		changeToStep('#colors');
+	});
+	$('#specials-point').click(function(){
+		changeToStep('#specials');
+	});
 });
+
+var changeToStep = function(selector){
+	var selectedPoint = selector + '-point'
+	$('.config-step').fadeOut('slow');
+	$(selector).fadeIn('slow');
+	$('.point').css('background-color', '#FFFFFF');
+	$(selectedPoint).css('background-color', '#000000');
+};
