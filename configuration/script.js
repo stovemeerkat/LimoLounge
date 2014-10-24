58f08fa9fde4230e00bb54527135078cb6b58db5
marvinheinzelmann <marvin.heinzelmann@t-online.de>
2014-10-24 17:12:07 +0200

    Revert "did some crap"
    This reverts commit e00420422499b2942c5613551b5d00119131ab0f.

---
+++ configuration/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('#flavors-point').click(function(){
+		changeToStep('#flavors');
+	});
+	$('#colors-point').click(function(){
+		changeToStep('#colors');
+	});
+	$('#specials-point').click(function(){
+		changeToStep('#specials');
+	});
+});
+
+var changeToStep = function(selector){
+	var selectedPoint = selector + '-point'
+	$('.config-step').fadeOut('slow');
+	$(selector).fadeIn('slow');
+	$('.point').css('background-color', 'rgba(255, 255, 255, 0)');
+	$(selectedPoint).css('background-color', '#000000');
+};
