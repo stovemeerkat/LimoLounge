073b0e264618f8fa791bf7ffdc37eb8d08a4909a
AegonTheConqueror <jonas@schneider-roeser.de>
2014-10-31 13:08:39 +0100

    Blablabla

--- configuration/script.js
+++ configuration/script.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
 
 var changeToStep = function(selector){
 	var selectedPoint = selector + '-point'
-	$('.config-step').fadeOut('slow');
-	$(selector).fadeIn('slow');
+	$('.config-step').hide();
+	$(selector).show();
 	$('.point').css('background-color', 'rgba(255, 255, 255, 0)');
 	$(selectedPoint).css('background-color', '#000000');
 };
