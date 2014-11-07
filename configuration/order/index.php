77fe5bf9f7aaa6918b4a053771f4005f14f742f8
AegonTheConqueror <jonas@schneider-roeser.de>
2014-11-07 20:18:37 +0100

    More optn php

--- configuration/order/index.php
+++ configuration/order/index.php
@@ -21,6 +21,7 @@
 	
 	<body>
 		<h1>Vielen Dank für ihre Bestellung.</h1>
+		<h2>Aromen</h2>
 		Zitrone: <?php
 			if(!empty($_POST['Zitrone'])){
 				echo 'ja';
@@ -29,6 +30,38 @@
 			}?><br>
 		Orange: <?php
 			if(!empty($_POST['Orange'])){
+				echo 'ja';
+			} else {
+				echo 'nein';
+			}?><br>
+		Passionsfrucht: <?php
+			if(!empty($_POST['Passionsfrucht'])){
+				echo 'ja';
+			} else {
+				echo 'nein';
+			}?><br>
+		<h2>Farbstoffe</h2>
+		Gelb: <?php
+			if(!empty($_POST['Gelb'])){
+				echo 'ja';
+			} else {
+				echo 'nein';
+			}?><br>
+		Rot: <?php
+			if(!empty($_POST['Rot'])){
+				echo 'ja';
+			} else {
+				echo 'nein';
+			}?><br>
+		<h2>Spezielle Zutaten</h2>
+		Kohlensäure: <?php
+			if(!empty($_POST['Kohlensäure'])){
+				echo 'ja';
+			} else {
+				echo 'nein';
+			}?><br>
+		Koffein: <?php
+			if(!empty($_POST['Koffein'])){
 				echo 'ja';
 			} else {
 				echo 'nein';
