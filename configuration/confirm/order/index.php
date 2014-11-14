ea9fc27b4ba62614522b42286884ae1f1e1bbb7a
AegonTheConqueror <jonas@schneider-roeser.de>
2014-11-14 22:14:31 +0100

    some alpha-satge design adjustments :D

--- configuration/confirm/order/index.php
+++ configuration/confirm/order/index.php
@@ -46,33 +46,43 @@
 	}
  ?>
  
- <html>
+<html>
 	<head>
 		<title>Vielen Dank!</title>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 	</head>
 	
-	<body>
-		<h2>Geschmack</h2>
-		<?php
-			checkOption('Apfel');
-			checkOption('Himbeere');
-			checkOption('Mandarine');
-			checkOption('Mango');
-			checkOption('Orange');
-			checkOption('Zitrone');
-		?>
-		<h2>Farbe</h2>
-		<?php
-			checkOption('Blau');
-			checkOption('Gelb');
-			checkOption('Gruen');
-			checkOption('Rot');
-			checkOption('Violett');
-		?>
-		<h2>Spezielle Zutaten</h2>
-		<?php
-			checkOption('Kohlensaeure');
-			checkOption('Koffein');
-		?>
+	<body style="color:white;">
+		<div id="shade"></div>
+		<div id="directory">
+			<a href="http://limolounge.16mb.com/"><div id="logo"><img id="logo-img" src="Bilder/Logo.png"></div></a>
+			<div class="dir-item"><a href="http://limolounge.16mb.com/" class="dir-link">WINTER</a></div>
+			<div class="dir-item"><a href="http://limolounge.16mb.com/configuration/" class="dir-link">IS</a></div>
+			<div class="dir-item"><a href="http://limolounge.16mb.com/configuration/" class="dir-link">COMING</a></div>
+		</div>
+			<div class="option-wrapper" style="z-index: 3">
+				<h2>Geschmack</h2>
+				<?php
+					checkOption('Apfel');
+					checkOption('Himbeere');
+					checkOption('Mandarine');
+					checkOption('Mango');
+					checkOption('Orange');
+					checkOption('Zitrone');
+				?>
+				<h2>Farbe</h2>
+				<?php
+					checkOption('Blau');
+					checkOption('Gelb');
+					checkOption('Gruen');
+					checkOption('Rot');
+					checkOption('Violett');
+				?>
+				<h2>Spezielle Zutaten</h2>
+				<?php
+					checkOption('Kohlensaeure');
+					checkOption('Koffein');
+				?>
 	</body>
+
 </html>
