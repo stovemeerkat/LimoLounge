777c72a0bee50b1e2a5651c66b7cd556c87f9c8f
AegonTheConqueror <jonas@schneider-roeser.de>
2014-11-15 13:03:22 +0100

    Updatet design of order

--- configuration/confirm/index.php
+++ configuration/confirm/index.php
@@ -21,7 +21,7 @@
 		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 	</head>
 	
-	<body style="color:white;">
+	<body>
 		<div id="shade"></div>
 		<div id="directory">
 			<a href="http://limolounge.16mb.com/"><div id="logo"><img id="logo-img" src="Bilder/Logo.png"></div></a>
@@ -29,31 +29,32 @@
 			<div class="dir-item"><a href="http://limolounge.16mb.com/configuration/" class="dir-link">IS</a></div>
 			<div class="dir-item"><a href="http://limolounge.16mb.com/configuration/" class="dir-link">COMING</a></div>
 		</div>
-			<div class="option-wrapper" style="z-index: 3">
-				<h1>Bestellung übeprüfen:</h1>
-				<h2>Geschmack</h2>
-				<?php
-					checkOption('Apfel');
-					checkOption('Himbeere');
-					checkOption('Mandarine');
-					checkOption('Mango');
-					checkOption('Orange');
-					checkOption('Zitrone');
-				?>
-				<h2>Farbe</h2>
-				<?php
-					checkOption('Blau');
-					checkOption('Gelb');
-					checkOption('Gruen');
-					checkOption('Rot');
-					checkOption('Violett');
-				?>
-				<h2>Spezielle Zutaten</h2>
-				<?php
-					checkOption('Kohlensaeure');
-					checkOption('Koffein');
-				?>
-				<a href="order/index.php?<?php echo htmlspecialchars(SID); ?>"><div class="button" id="btn-Finished">Bestellen</div></a>
+		<div class="config-step" style="z-index: 3">
+			<h1>Bestellung übeprüfen:</h1>
+			<h2>Geschmack</h2>
+			<?php
+				checkOption('Apfel');
+				checkOption('Himbeere');
+				checkOption('Mandarine');
+				checkOption('Mango');
+				checkOption('Orange');
+				checkOption('Zitrone');
+			?>
+			<h2>Farbe</h2>
+			<?php
+				checkOption('Blau');
+				checkOption('Gelb');
+				checkOption('Gruen');
+				checkOption('Rot');
+				checkOption('Violett');
+			?>
+			<h2>Spezielle Zutaten</h2>
+			<?php
+				checkOption('Kohlensaeure');
+				checkOption('Koffein');
+			?>
+			<a href="order/index.php?<?php echo htmlspecialchars(SID); ?>"><div class="button" id="btn-Finished">Bestellen</div></a>
+		</div>
 	</body>
 
 </html>
