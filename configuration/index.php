e6570baffccf82bd6e5d064ba5f135e9d9247566
marvinheinzelmann <marvin.heinzelmann@t-online.de>
2014-11-21 17:07:42 +0100

    improved password page

--- configuration/index.php
+++ configuration/index.php
@@ -9,21 +9,23 @@
 		<h1>Diese Seite ist noch im Entwicklungsstadium.</h1>
 		<p>Sie können daher noch keine Bestellungen aufgeben.</p>
 		<p>Kommen sie später noch einmal. Bestellungen dürften vorraussichtlich ab dem 10. Dezember möglich sein</p>
-		
-		<?php
-			$password = '';
-			if(!$_POST['input'] == $password){
-				?>
-				<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-					<input type="password" name="input">
-				</form>
-				<?php
-			} else {
-				?>
-				Das Passwort war richtig!
-				<a href="dev_index.html">Weiter zur Konfigurationsseite</a>
-				<?php
-			}
-		?>
+		<div>
+			<p>Sollten sie an der Entwicklung dieser Seite beteiligt sein, geben sie hier das Passwort ein:</p>
+			<?php
+				$password = '';
+				if(!$_POST['input'] == $password){
+					?>
+					<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+						<input type="password" name="input">
+					</form>
+					<?php
+				} else {
+					?>
+					Das Passwort war richtig!
+					<a href="dev_index.html">Weiter zur Konfigurationsseite</a>
+					<?php
+				}
+			?>
+		</div>
 	</body>
 </html>
