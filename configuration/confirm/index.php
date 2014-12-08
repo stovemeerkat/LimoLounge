5a4f4f796560c1bc4d1156e6cdb759162fa9f6f4
AegonTheConqueror <jonas@schneider-roeser.de>
2014-12-08 16:50:27 +0100

    heeeeeellllllppppppp

--- configuration/confirm/index.php
+++ configuration/confirm/index.php
@@ -32,37 +32,47 @@
 		</div>
 		<div class="wrapper">
 			<div id="header"><p>Bestellung übeprüfen:</p></div>
-			<h2>Geschmack</h2>
-			<?php
-				checkOption('Apfel');
-				checkOption('Himbeere');
-				checkOption('Mandarine');
-				checkOption('Mango');
-				checkOption('Orange');
-				checkOption('Zitrone');
-			?>
-			<h2>Farbe</h2>
-			<?php
-				checkOption('Blau');
-				checkOption('Gelb');
-				checkOption('Gruen');
-				checkOption('Rot');
-				checkOption('Violett');
-			?>
-			<h2>Spezielle Zutaten</h2>
-			<?php
-				checkOption('Kohlensaeure');
-				checkOption('Koffein');
-			?>
-			<h2>Zahlungsdaten</h2>
-			<form action="order/index.php" method="post">
-				<table>
-					<tr><td></ts></tsY><input type="text" name="Name" class="inputLabel" placeholder="Vorname, Nachname" required /></td></tr>
-					<tr><td></ts></tsY><input type="text" name="Strasse" class="inputLabel" placeholder="Straße, Hausnr." required /></td></tr>
-					<tr><td></ts></tsY><input type="text" name="Ort" class="inputLabel" placeholder="PLZ, Ort" required /></td></tr>
-					<tr><td><button type="submit" class="button"><div style="display: table-cell; vertical-align: middle">Abschicken</div></button></td></tr>
-				</table>
-			</form>
+			<div id="smallWrapper">
+				<h2>Geschmack</h2>
+				<div class="verysmallWrapper">
+					<?php
+						checkOption('Apfel');
+						checkOption('Himbeere');
+						checkOption('Mandarine');
+						checkOption('Mango');
+						checkOption('Orange');
+						checkOption('Zitrone');
+					?>
+				</div>
+				<h2>Farbe</h2>
+				<div class="verysmallWrapper">
+					<?php
+						checkOption('Blau');
+						checkOption('Gelb');
+						checkOption('Gruen');
+						checkOption('Rot');
+						checkOption('Violett');
+					?>
+				</div>
+				<h2>Spezielle Zutaten</h2>
+				<div class="verysmallWrapper">
+					<?php
+						checkOption('Kohlensaeure');
+						checkOption('Koffein');
+					?>
+				</div>
+				<h2>Zahlungsdaten</h2>
+				<div class="verysmallWrapper">
+					<form action="order/index.php" method="post">
+						<table>
+							<tr><td></ts></tsY><input type="text" name="Name" class="inputLabel" placeholder="Vorname, Nachname" required /></td></tr>
+							<tr><td></ts></tsY><input type="text" name="Strasse" class="inputLabel" placeholder="Straße, Hausnr." required /></td></tr>
+							<tr><td></ts></tsY><input type="text" name="Ort" class="inputLabel" placeholder="PLZ, Ort" required /></td></tr>
+							<tr><td><button type="submit" class="button"><div style="display: table-cell; vertical-align: middle;">Abschicken</div></button></td></tr>
+						</table>
+					</form>
+				</div>
+			</div>
 		</div>
 		<div id="impressum">
 			<p class="impr-paragraph" id="impr-heading">Impressum</p>
